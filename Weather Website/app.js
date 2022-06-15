15f24a13bbde89aac93f8b1c864627c4d2088659
abdulqadir18 <boxwalaabdulqadir2002@gmail.com>
2022-06-15 13:58:47 +0530

    Added post functionality to weather website

--- Weather Website/app.js
+++ Weather Website/app.js
@@ -5,9 +5,19 @@ const app = express();
 //adding https
 const https = require("https");
 
+//adding body-Parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended : true}));
+
 app.get("/", function(req,res){
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=indore&units=metric&appid=a571b38d518a85b624b171bd5209fc05#";
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res){
+  const city = req.body.cityName;
+  const appKey = "a571b38d518a85b624b171bd5209fc05";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+appKey+"&units=metric";
   https.get(url, function(response){
     console.log(response.statusCode);
     response.on("data", function(data){
@@ -19,7 +29,7 @@ app.get("/", function(req,res){
       console.log(weatherDescription);
 
       res.write("<p>Weather here is " + weatherDescription + "</p>");
-      res.write("<h1>The temparature is " + temparature + " degree Celsius</h1>");
+      res.write("<h1>The temparature in " + city + " is " + temparature + " degree Celsius</h1>");
       res.write("<img src='http://openweathermap.org/img/wn/"+weatherImage+"@2x.png'>");
       res.send();
     });
